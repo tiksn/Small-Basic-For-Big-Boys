825fdd34878b51a7976defbd092ffd016307695d
Tigran TIKSN Torosyan <personal@tiksn.com>
2018-07-21 23:43:38 +0300

    added random big integer generator

---
+++ SmallBasicForBigBoys/BigBoys.vb
@@ -0,0 +1,17 @@
+﻿
+Imports System.Numerics
+Imports Microsoft.SmallBasic.Library
+
+<SmallBasicType()>
+Public Module BigBoys
+    Public Function GetRandomBigInteger(ByVal Length As Integer) As BigInteger
+        Dim Result As BigInteger = BigInteger.Zero
+        Dim RNG As New Random
+
+        For index = 1 To Length
+            Dim NextDigit = RNG.Next(10)
+            Result *= 10
+            Result += NextDigit
+        Next
+    End Function
+End Module
