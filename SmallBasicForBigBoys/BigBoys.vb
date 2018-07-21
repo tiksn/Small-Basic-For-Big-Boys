2ae8d563acad246beb64c9cf7a07ec03ba74f37c
Tigran TIKSN Torosyan <personal@tiksn.com>
2018-07-22 00:41:22 +0300

    fixed issue

--- SmallBasicForBigBoys/BigBoys.vb
+++ SmallBasicForBigBoys/BigBoys.vb
@@ -5,17 +5,18 @@ Imports Microsoft.SmallBasic.Library
 <SmallBasicType()>
 Public Module BigBoys
 
-    Public Function GetRandomBigInteger(ByVal Length As Integer) As BigInteger
+    Public Function GetRandomBigInteger(ByVal Length As Primitive) As Primitive
         Dim Result As BigInteger = BigInteger.Zero
         Dim RNG As New Random
+        Dim L As Integer = Length
 
-        For index = 1 To Length
+        For index = 1 To L
             Dim NextDigit = RNG.Next(10)
             Result *= 10
             Result += NextDigit
         Next
 
-        Return Result
+        Return Result.ToString()
     End Function
 
 End Module
